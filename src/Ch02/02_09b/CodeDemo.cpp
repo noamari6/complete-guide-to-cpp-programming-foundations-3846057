6e2c8c3fb07202f2d808279973077942e0e7aeb3
noamari6 <noamari6@gmail.com>
2025-09-13 19:43:43 +0000

    complete 02_09b

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,8 +7,17 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character {
+    std::string name;
+    int level;
+    character_role role;
+};
 int main(){
-    
+    game_character player;
+    player.name = "Noam";
+    player.level= 11;
+    player.role = character_role::npc;
+    std::cout << player.name << " is a level " << player.level << " and his role is " << (int)player.role << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
