b40bc85395aec1764ee304a9a339be3cf89b2499
noamari6 <noamari6@gmail.com>
2025-09-17 13:46:07 +0000

    complete 04_07b

--- src/Ch04/04_07b/CodeDemo.cpp
+++ src/Ch04/04_07b/CodeDemo.cpp
@@ -7,7 +7,19 @@
 #include <complex>
 
 int main(){
-    
+    std::vector<std::complex<double>> viewers;
+    viewers.push_back(std::complex<double>(3.2, 4.2));
+    viewers.push_back(std::complex<double>(2.3, 9.2));
+    viewers.push_back(std::complex<double>(5.2, 1.2));
+    viewers.push_back(std::complex<double>(7.2, 3.2));
+    std::cout << "The first real part: " << viewers.begin()->real() <<std::endl;
+    std::cout << "Imaginary part at index 1: " << viewers[1].imag() <<std::endl;
+    std::cout << "Next to last real: " << prev(viewers.end(), 2)->real() <<std::endl;
+    std::cout << "Last imaginary: " << (viewers.end() -1)->imag() <<std::endl;
+
+
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
