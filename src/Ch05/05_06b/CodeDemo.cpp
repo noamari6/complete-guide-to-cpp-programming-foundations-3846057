db28db0467ca23e48c6a1541b7d16a687fe33fd6
noamari6 <noamari6@gmail.com>
2025-09-21 11:18:06 +0000

    complete 05_06b

--- src/Ch05/05_06b/CodeDemo.cpp
+++ src/Ch05/05_06b/CodeDemo.cpp
@@ -8,6 +8,11 @@
 int main(){
     std::vector<int> lapTimes = {92, 85, 88, 90, 87};
     float average;
+    for(int i=0; i < lapTimes.size(); i++)
+        average += lapTimes[i];
+    average /= lapTimes.size();
+
+    std::cout << "The average lap time is: " << average << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
