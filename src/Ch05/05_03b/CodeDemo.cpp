70219992e818549745fed4f70a1b167f627d2aad
noamari6 <noamari6@gmail.com>
2025-09-21 11:14:55 +0000

    complete 05_03b

--- src/Ch05/05_03b/CodeDemo.cpp
+++ src/Ch05/05_03b/CodeDemo.cpp
@@ -5,18 +5,30 @@
 #include <iostream>
 
 int main(){
-    float operand_1, operand_2, result;
+    float op1, op2, result;
     char operation;
 
     std::cout << "Enter operand 1: " << std::flush;
-    std::cin >> operand_1;
+    std::cin >> op1;
     std::cout << "Enter operand 2: " << std::flush;
-    std::cin >> operand_2;
+    std::cin >> op2;
     std::cout << "Choose operation [ + - * / ]: " << std::flush;
     std::cin >> operation;
 
-    // switch goes here
-
+    switch (operation) {
+        case '+':
+            result = op1 + op2;
+            break;
+        case '-':
+            result = op1-op2;
+            break;
+        case '*':
+            result = op1*op2;
+            break;
+        case '/':
+            result = op1/op2;
+            break;
+    }
     std::cout << "The result is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
